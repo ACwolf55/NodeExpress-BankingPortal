930b662b1095b9b409ddca5caaa9aee9e8b96c25
ACwolf55 <66705643+ACwolf55@users.noreply.github.com>
2021-07-23 15:00:09 -0700

    index.ejs

--- src/app.js
+++ src/app.js
@@ -3,4 +3,11 @@ const path = require ('path')
 const express = require('express')
 const app = express()
 
+app.set('views',path.join(__dirname,'views'))
+app.set('views engine','ejs')
 
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/',(req,res)=> res.render('index',{title:'index'}))
+
+app.listen(3000,()=>{ console.log('PS PRoject Running on port 3000')})
