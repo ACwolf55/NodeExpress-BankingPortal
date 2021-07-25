8bcc18f9540fa7ef580eb5e20034a2a43edbc59d
ACwolf55 <66705643+ACwolf55@users.noreply.github.com>
2021-07-24 22:09:58 -0700

    view engine

--- src/app.js
+++ src/app.js
@@ -1,13 +1,14 @@
 const fs = require('fs')
+const chalk = require('chalk')
 const path = require ('path')
 const express = require('express')
 const app = express()
 
 app.set('views',path.join(__dirname,'views'))
-app.set('views engine','ejs')
+app.set('view engine','ejs')
 
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/',(req,res)=> res.render('index',{title:'index'}))
 
-app.listen(3000,()=>{ console.log('PS PRoject Running on port 3000')})
+app.listen(3000,()=>{ console.log(chalk.blue('PS PRoject Running on port 3000'))})
